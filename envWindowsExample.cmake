1e2a6af86e335f365291a88a6d1990cbe62f424a
Farokh <farokhbahalou@yahoo.com>
2023-04-02 00:00:58 -0400

    first

---
+++ envWindowsExample.cmake
@@ -0,0 +1,9 @@
+set(GLFW_PATH ${CMAKE_CURRENT_SOURCE_DIR}/external/GLFW)
+set(GLM_PATH ${CMAKE_CURRENT_SOURCE_DIR}/external/glm)
+set(VULKAN_SDK_PATH  ${CMAKE_CURRENT_SOURCE_DIR}/external/VulkanSDK/1.3.236.0)
+ 
+# Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
+# set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
+ 
+# Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
+# set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Simple Vulkan Graphics Engine
+
+Graphics programming with the Vulkan API.
